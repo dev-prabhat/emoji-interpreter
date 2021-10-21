d3afff2d7fc703201ca9655ac736cf08ae4eae3a
Prabhat Singh <81572524+dev-prabhat@users.noreply.github.com>
2021-10-21 22:30:54 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var emoji = {
+  "😕": "Confused Face",
+  "😮": "Face with Open Mouth",
+  "😨": "Fearful Face",
+  "😢": "Crying Face",
+  "😎": "Smiling Face with Sunglasses",
+  "😴": "Sleeping Face"
+};
+
+var emojiArray = Object.keys(emoji);
+
+export default function App() {
+  const [meaning, setMeaning] = useState("");
+  var inputData, Means;
+
+  function emojiInterpreter(event) {
+    inputData = event.target.value;
+    Means = emoji[inputData];
+    setMeaning(Means);
+  }
+
+  function handleClick(item) {
+    inputData = item;
+    Means = emoji[inputData];
+    setMeaning(Means);
+  }
+
+  return (
+    <div className="App">
+      <h1>Emoji Interpreter </h1>
+      <input onChange={emojiInterpreter}></input>
+      <div className="meaning">{meaning}</div>
+      <ul>
+        {emojiArray.map(function (item, index) {
+          return (
+            <li
+              className="emoticon"
+              key={index}
+              onClick={() => handleClick(item)}
+            >
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
